d2ee403b0d6217b25f1e4066dcc0cfa0fe9450cb
Matthew Bazzo <matthewbazzo@gmail.com>
2023-12-22 20:47:11 -0500

    feat: add pen color and grid resolution logic
    Added logic to color grid based on input selection. Logic functions for
    black and random color.
    Added logic to update resolution indicator for canvas.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const STARTING_DIM = 16;
 let currentDIM = STARTING_DIM;
 let elemCanvas = document.getElementById('canvas');
 let elemInput = document.getElementById('input');
+let elemColorSettingBlack = document.getElementById('black');
+let elemColorSettingRandom = document.getElementById('random');
+let elemColorSettingDarkening = document.getElementById('darkening');
+let elemResTxt = document.getElementById('res-txt');
 
 /**
  * ADD EVENT LISTENERS
@@ -15,13 +19,17 @@ addEventListener('DOMContentLoaded', setUpCanvas);
 
 elemCanvas.addEventListener('mouseover', (event) => {
     if(event.target.id !== 'canvas') {
-        event.target.classList.add('pixel-colored');
+        if (elemColorSettingBlack.checked) event.target.style.backgroundColor = 'black';
+        if (elemColorSettingRandom.checked) {
+            event.target.style.backgroundColor = `rgb(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)})`
+        }
     }
 });
 
 elemInput.addEventListener('input', (event) => {
     if(event.target.id === 'grid-size') {
         currentDIM = event.target.value;
+        elemResTxt.textContent = `Current Resolution = ${currentDIM} x ${currentDIM}`
         clearCanvas();
         setUpCanvas();
     }
