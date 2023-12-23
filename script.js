a44437d9dbfbf4538ddb224f163c397bb74f9e5e
Matthew Bazzo <matthewbazzo@gmail.com>
2023-12-22 21:32:55 -0500

    feat: add darkening effect logic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
  */
 
 const STARTING_DIM = 16;
+const DARKENING_DECREMENT = 26
 let currentDIM = STARTING_DIM;
 let elemCanvas = document.getElementById('canvas');
 let elemInput = document.getElementById('input');
@@ -19,9 +20,27 @@ addEventListener('DOMContentLoaded', setUpCanvas);
 
 elemCanvas.addEventListener('mouseover', (event) => {
     if(event.target.id !== 'canvas') {
-        if (elemColorSettingBlack.checked) event.target.style.backgroundColor = 'black';
+        if (elemColorSettingBlack.checked) event.target.style.backgroundColor = 'rgb(0,0,0)';
+        
         if (elemColorSettingRandom.checked) {
             event.target.style.backgroundColor = `rgb(${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)})`
+        }
+
+        if (elemColorSettingDarkening.checked) {
+            
+            if(event.target.style.backgroundColor) { // in case color is undefined
+                let rVal, gVal, bVal;
+                [rVal, gVal, bVal] = event.target.style.backgroundColor.split("(")[1].split(")")[0].split(", ");
+    
+                if (parseInt(rVal) + parseInt(gVal) + parseInt(bVal) !== 0) { // if not black
+                    let rValNew = Math.max(0,parseInt(rVal)-DARKENING_DECREMENT);
+                    let gValNew = Math.max(0,parseInt(gVal)-DARKENING_DECREMENT);
+                    let bValNew = Math.max(0,parseInt(bVal)-DARKENING_DECREMENT);
+                    event.target.style.backgroundColor = `rgb(${rValNew},${gValNew},${bValNew})`;
+                }
+            } else {
+                event.target.style.backgroundColor = `rgb(${255 - DARKENING_DECREMENT},${255 - DARKENING_DECREMENT},${255 - DARKENING_DECREMENT})`;
+            }
         }
     }
 });
@@ -46,7 +65,6 @@ function clearCanvas() {
 }
 
 function setUpCanvas() {
-
     for (let i = 0; i < currentDIM; i++) {
         let newCol = document.createElement('div');
         newCol.width = `${elemCanvas.width / currentDIM}%`;
